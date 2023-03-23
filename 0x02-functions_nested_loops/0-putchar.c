8dabd126fa85386679e0212f6a1ead0200770c32
deraa123 <chideraaevans@gmail.com>
2023-03-23 06:24:51 -0700

    all task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * main - prints string "_putchar"
- *
- *
- * Return: Always return 0
+ * main - starting point
+ * Description - prints _putchar followed by a new line
+ * Return: always 0
  */
+
 int main(void)
 {
-		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
