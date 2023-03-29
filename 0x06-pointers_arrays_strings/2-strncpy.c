19d0ab39956e92fc8c003606063d5706fe372353
deraa123 <chideraaevans@gmail.com>
2023-03-29 08:58:35 -0700

    all task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,35 @@
+@el898
+el898 srncpy
+ 1 contributor
+31 lines (17 sloc)  342 Bytes
+#include "main.h"
+/**
+ *_strncpy - finds the length of the string
+ * @dest: pointer to the string
+ * @src: pointer
+ * @n: int
+ * Return: destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+
+		dest[i] = src[i];
+
+
+
+	for ( ; i < n; i++)
+
+		dest[i] = '\0';
+
+
+
+	return (dest);
+
+}
